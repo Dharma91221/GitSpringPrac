a8e0a6460a38236479252f046e798c86680440b1
Dharma91221 <Dharmarao91221@gmail.com>
2021-04-16 10:15:36 +0530

    spring git added

---
+++ src/main/java/com/example/demo/SpringGitPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitPracApplication.class, args);
+	}
+
+}
